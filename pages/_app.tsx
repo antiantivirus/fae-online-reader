34e29d5515af1a30ec947b5dec926b32eaa5c79e
Jack MB <jack@designandcode.com>
2024-02-29 12:06:07 +0100

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+
+const abc = localFont({
+  src: "../public/fonts/ABCDiatype-Regular.woff2",
+  variable: "--font-abc",
+});
+const abcItalic = localFont({
+  src: "../public/fonts/ABCDiatype-RegularItalic.woff2",
+  variable: "--font-abc-italic",
+});
+const newEdge = localFont({
+  src: "../public/fonts/NewEdgeSoftPower4-LightRounded.woff2",
+  variable: "--font-new-edge",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main
+      className={`${abc.variable} ${abcItalic.variable} ${newEdge.variable} font-sans`}
+    >
+      <Component {...pageProps} />
+    </main>
+  );
 }
