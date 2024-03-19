c64ba949d7005bb6e16db71fc821b753f2a62f8c
Jack MB <jack@designandcode.com>
2024-03-19 13:29:27 +0000

    add fathom analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,34 @@
 import "@/styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
 import type { AppProps } from "next/app";
-import Header from "@/components/header";
-import TOC from "@/components/toc";
 import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
+    Fathom.load("ONSXGREG", {
+      includedDomains: ["reader.futureartecosystems.org"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
   return (
     <ThemeProvider
       defaultTheme="light"
