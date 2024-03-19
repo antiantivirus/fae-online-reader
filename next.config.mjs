c0e6f373ae0942e44cb8b6af4289135c7541b169
Jack MB <jack@designandcode.com>
2024-03-19 12:32:17 +0000

    redirect / to homepage

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   transpilePackages: ["gsap"],
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/briefings/fae4",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
