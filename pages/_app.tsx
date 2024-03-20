2cd654d0d59b8c786b9abf38f9fae736b770b97d
Jack MB <jack@designandcode.com>
2024-03-20 15:06:40 +0000

    add speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import * as Fathom from "fathom-client";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import Header from "@/components/header";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
