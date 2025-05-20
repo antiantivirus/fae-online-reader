864b3f027f968d2505ae7287fae6b5df3dd25247
Jack MB <hi@jackmurraybrown.com>
2025-05-20 15:10:31 +0200

    fix gsap timeline on picture

--- components/fae_five/picture.tsx
+++ components/fae_five/picture.tsx
@@ -13,23 +13,23 @@ export default function Picture({ main, bg, alt }: {
   useGSAP(() => {
     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.to("#bg-image", {
-      opacity: 1,
+    const tl = gsap.timeline({
       scrollTrigger: {
         trigger: "#main-image",
         start: "top bottom",
         end: "bottom top",
-        toggleActions: "play reverse play reverse",
+        scrub: 1,
       },
-      duration: 0.5,
-      ease: "power1.out",
     });
+
+    tl.fromTo("#bg-image", { opacity: 0 }, { opacity: 1, ease: "none" })
+      .to("#bg-image", { opacity: 0, ease: "none" });
   });
 
   return (
     <Box wide>
       <div className="diagram">
-        <Image width={880} height={300} id="main-image" src={main} alt={alt} />
+        <Image width={880} height={300} id="main-image" className="z-50 relative" src={main} alt={alt} />
         <Image width={880} height={300} id="bg-image" src={bg} alt="" className="fixed top-0 left-0 w-full h-full object-cover opacity-0 pointer-events-none" />
       </div>
     </Box>
